0ca0a84f3991cdbad0a8969c84e3036c99ac804b
Curro Portillo Guerrero <cporgue2307@g.educaand.es>
2025-11-11 10:52:27 +0100

    ejercicio 53

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int altura = Integer.parseInt(System.console().readLine("Introduzca la altura de la figura: "));
+        int fila = altura;
+        int base = 0;
+
+        do{
+            for(int i = 0; i < fila; i++){
+                System.out.print("*");
+            }
+            System.out.println();
+            base++;
+            fila--;
+        }while(base < altura);
     }
 }
